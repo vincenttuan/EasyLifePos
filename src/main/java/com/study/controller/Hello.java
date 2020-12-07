78fc3b734c0a40f921f671adec8b9306e7061d63
vincenttuan <vincentjava@yahoo.com.tw>
2020-12-07 16:02:01 +0800

    SSH

--- src/main/java/com/study/controller/Hello.java
+++ src/main/java/com/study/controller/Hello.java
@@ -1,6 +1,24 @@
 package com.study.controller;
 
+import java.util.Date;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
+@RequestMapping("/hello")
 public class Hello {
+    
+    @RequestMapping("/now")
+    @ResponseBody
+    public String now() {
+        return "now : " + new Date();
+    }
+    
+    @RequestMapping("/now_page")
+    public String nowPage() {
+        return "now_page"; // 執行 now_page.jsp
+    }
+    
     
 }
---
+++ src/main/webapp/WEB-INF/view/now_page.jsp
@@ -0,0 +1,18 @@
+<%-- 
+    Document   : now_page
+    Created on : 2020/12/7, 下午 03:57:10
+    Author     : vincenttuan
+--%>
+
+<%@page import="java.util.Date"%>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1><%=new Date() %></h1>
+    </body>
+</html>
