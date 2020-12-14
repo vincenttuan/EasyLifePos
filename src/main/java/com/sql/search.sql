c1b3d31def7e02c2c0bdda5bebadc4b189c7e678
vincenttuan <vincentjava@yahoo.com.tw>
2020-12-14 16:53:08 +0800

    SSH

--- src/main/java/com/sql/search.sql
+++ src/main/java/com/sql/search.sql
@@ -11,3 +11,29 @@ WHERE m.mid = r.mid AND a.aid = r.aid AND a.aname = 'B2B';
 SELECT 'B2B' as ATTR, count(m.maccount) as 'COUNT'
 FROM Member m, MemberAttr a, MemberAttrRef r
 WHERE m.mid = r.mid AND a.aid = r.aid AND a.aname = 'B2B';
+
+-- B2B, B2C, C2C 的會員數量各有(Part I) ?
+SELECT 'B2B' as ATTR, count(m.maccount) as 'COUNT'
+FROM Member m, MemberAttr a, MemberAttrRef r
+WHERE m.mid = r.mid AND a.aid = r.aid AND a.aname = 'B2B'
+UNION ALL
+SELECT 'B2C' as ATTR, count(m.maccount) as 'COUNT'
+FROM Member m, MemberAttr a, MemberAttrRef r
+WHERE m.mid = r.mid AND a.aid = r.aid AND a.aname = 'B2C'
+UNION ALL
+SELECT 'C2C' as ATTR, count(m.maccount) as 'COUNT'
+FROM Member m, MemberAttr a, MemberAttrRef r
+WHERE m.mid = r.mid AND a.aid = r.aid AND a.aname = 'C2C';
+
+-- B2B, B2C, C2C 的會員數量各有(Part II) ?
+SELECT a.aname, count(m.maccount) as 'COUNT'
+FROM Member m, MemberAttr a, MemberAttrRef r
+WHERE m.mid = r.mid AND a.aid = r.aid
+GROUP BY a.aname;
+
+-- B2B, B2C, C2C 哪一個會員數量最多
+SELECT a.aname, count(m.maccount) as 'COUNT'
+FROM Member m, MemberAttr a, MemberAttrRef r
+WHERE m.mid = r.mid AND a.aid = r.aid
+GROUP BY a.aname
+ORDER BY COUNT DESC limit 1;
